33de9e60aa32f99ff55579007dbc3e3e438a3db8
Akrommkeren <akmaldendi41@gmail.com>
2025-06-10 00:40:37 +0700

    first commit

--- resources/views/Contact.blade.php
+++ resources/views/Contact.blade.php
@@ -143,7 +143,7 @@
       <div class="container">
          <div class="copyright-area">
             <p>
-               Copyright @<script>document.write(new Date().getFullYear())</script> <a href="{{ route('index') }}">Darulirfan</a>
+               Copyright @<script>document.write(new Date().getFullYear())</script> <a href="{{ route('index') }}">FasDana</a>
             </p>
          </div>
       </div>
