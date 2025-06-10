f4dfbcbe80e723cb6e64278ee1cdba5f621a1062
SaharRiflah <sahariflah123@gmail.com>
2025-06-10 22:36:43 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+// Mengimpor berbagai fitur dari Laravel dan package tambahan
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -9,15 +10,13 @@ use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 use Spatie\Activitylog\Traits\LogsActivity;
 
+// Model User mewakili data pengguna sistem
 class User extends Authenticatable
 {
+    // Mengaktifkan fitur bawaan seperti notifikasi, role, dan pencatatan aktivitas
     use HasFactory, Notifiable, HasRoles, LogsActivity;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    // Kolom yang boleh diisi
     protected $fillable = [
         'name',
         'username',
@@ -25,40 +24,31 @@ class User extends Authenticatable
         'avatar'
     ];
 
+    // Konfigurasi log aktivitas pengguna
     protected static $logAttributes = ['name', 'username'];
-
     protected static $igonoreChangedAttributes = ['updated_at'];
-
     protected static $recordEvents = ['created', 'updated', 'deleted'];
-
     protected static $logOnlyDirty = true;
-
     protected static $logName = 'user';
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+    // Kolom yang disembunyikan saat data ditampilkan
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
+    // Konversi otomatis tipe data tertentu
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
 
+    // Menyediakan deskripsi khusus saat aktivitas (create, update, delete) terjadi
     public function getDescriptionForEvent(string $eventName): string
     {
         return "You have {$eventName} user";
     }
 
+    // Menandai apakah pengguna memiliki peran Admin
     public function getIsAdminAttribute()
     {
         return $this->hasRole('Admin');
