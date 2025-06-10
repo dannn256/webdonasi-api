522be128b66d07072168098e18c24d69cabed4b5
SaharRiflah <sahariflah123@gmail.com>
2025-06-10 22:33:49 +0700

    Update Contact.php

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+// Mengimpor fitur bawaan Laravel
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Model Contact mewakili data pesan dari pengguna
 class Contact extends Model
 {
     use HasFactory;
 
+    // Daftar kolom yang bisa diisi
     protected $fillable = [
         'subject',
         'message',
@@ -18,6 +21,7 @@ class Contact extends Model
         'status'
     ];
 
+    // Relasi: satu pesan bisa memiliki banyak balasan
     public function replies()
     {
         return $this->hasMany(Reply::class);
