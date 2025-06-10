afbe730a53b754acde0f62db6f1f9f26a8f5ab96
SaharRiflah <sahariflah123@gmail.com>
2025-06-10 22:34:50 +0700

    Update Donation.php

--- app/Models/Donation.php
+++ app/Models/Donation.php
@@ -1,14 +1,17 @@
-<?php
+<?php 
 
 namespace App\Models;
 
+// Mengimpor fitur bawaan Laravel
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Model Donation mewakili data donasi dari pengguna
 class Donation extends Model
 {
     use HasFactory;
 
+    // Daftar kolom yang bisa diisi
     protected $fillable = [
         'name',
         'address',
@@ -20,6 +23,7 @@ class Donation extends Model
         'payment_id'
     ];
 
+    // Relasi: setiap donasi terhubung ke satu kampanye
     public function campaign()
     {
         return $this->belongsTo(Campaign::class);
