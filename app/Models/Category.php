1762fbda38b1f63da146c40c47b50296437adc11
SaharRiflah <sahariflah123@gmail.com>
2025-06-10 22:32:18 +0700

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,18 +2,23 @@
 
 namespace App\Models;
 
+// Mengimpor fitur bawaan Laravel
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Mendefinisikan model Category, mewakili data kategori di database
 class Category extends Model
 {
+    // Mengaktifkan fitur factory untuk pembuatan data palsu saat testing
     use HasFactory;
 
+    // Menentukan kolom yang boleh diisi saat membuat atau mengubah data kategori
     protected $fillable = [
         'name',
         'slug'
     ];
 
+    // Relasi: Satu kategori bisa memiliki banyak postingan
     public function posts()
     {
         return $this->hasMany(Post::class);
